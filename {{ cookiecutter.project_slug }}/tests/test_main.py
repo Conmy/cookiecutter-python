731323bab9e49a9ec90b71e2bd0b37a8427b2e63
David Conmy <david@conmy.me>
2026-04-21 16:17:38 +0100

    fix: remove unneeded --active flags in makefile

--- {{ cookiecutter.project_slug }}/tests/test_main.py
+++ {{ cookiecutter.project_slug }}/tests/test_main.py
@@ -1,7 +1,8 @@
 from {{ cookiecutter.package_name }}.main import main
+from typing import Any
 
 
-def test_main(capsys):
+def test_main(capsys: Any) -> None:
     main()
     captured = capsys.readouterr()
     assert "Hello from" in captured.out
